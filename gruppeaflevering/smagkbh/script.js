0526ac61aa008fedd02ca8c4d9cbf6b1896a38c5
Annemette <annemette@MacBook-Pro.local>
2020-09-11 12:20:33 +0200

    array, filter og popop virker

--- gruppeaflevering/smagkbh/script.js
+++ gruppeaflevering/smagkbh/script.js
@@ -1,74 +1,88 @@
-        let filter = "alle";
-        let retter;
+//variabler/konstanter
+let filter = "alle";
+let steder;
 
-        document.addEventListener("DOMContentLoaded", loadJSON);
-        const popop = document.querySelector("#popop");
+//funktionen loadJSON kaldes, når DOMen er lukket
+document.addEventListener("DOMContentLoaded", loadJSON);
+const popop = document.querySelector("#popop");
 
-        async function loadJSON() {
-            const JSONData = await fetch("https://spreadsheets.google.com/feeds/list/17Dd7DvkPaFamNUdUKlrFgnH6POvBJXac7qyiS6zNRw0/od6/public/values?alt=json");
-            retter = await JSONData.json();
-            addEventListenersToButtons();
-            visRetter();
+
+//json henter data fra google sheet. indholdet sættes = funktion steder
+async function loadJSON() {
+
+    const JSONData = await fetch("https://spreadsheets.google.com/feeds/list/1HM38zJiPVJez_N7FT8A_-ysQwbudtVIVaQspUv_v9jY/od6/public/values?alt=json");
+
+    steder = await JSONData.json();
+
+    //funktioner kaldes
+    addEventListenersToButtons();
+    visSteder();
+}
+
+
+function visSteder() {
+    console.log("funktion steder");
+
+    //konstanter for liste og template
+    const list = document.querySelector("#liste");
+    const stederTemplate = document.querySelector("template");
+
+    //indholdet i konstanten list bliver nulstillet
+    list.innerHTML = "";
+
+
+    //steder-array bliver gennemgået sted for sted
+    steder.feed.entry.forEach(sted => {
+        if (filter == "alle" || filter == sted.gsx$kategori.$t.toLowerCase()) {
+            let clone = stederTemplate.cloneNode(true).content;
+
+          //det nye indhold bliver placeret
+            clone.querySelector(".navn").textContent = sted.gsx$navn.$t;
+            clone.querySelector(".kort").textContent = sted.gsx$kort.$t;
+           // clone.querySelector("img").src = "imgs/small/" + ret.gsx$billede.$t + "-sm.jpg";
+
+
+           //når der klikkes på vores article-tag kaldes funktionen visDetaljer
+ clone.querySelector("article").addEventListener("click", () => visDetaljer(sted));
+
+            list.appendChild(clone);
+            console.log(sted);
+
         }
+    })
+}
+
+//funktion indsætter data i vores popop
+function visDetaljer(sted) {
+    console.log(sted);
+    popop.querySelector("h2").textContent = sted.gsx$navn.$t;
+    //popop.querySelector("img").src = "imgs/small/" + ret.gsx$billede.$t + "-sm.jpg";
+    popop.querySelector(".lang").textContent = sted.gsx$lang.$t;
+    popop.style.display = "block";
+
+}
 
 
-        function visRetter() {
-            const list = document.querySelector("#liste");
-            const menuTemplate = document.querySelector("template");
-            list.innerHTML = "";
-
-            retter.feed.entry.forEach(ret => {
-                if (filter == "alle" || filter == ret.gsx$kategori.$t.toLowerCase()) {
-                    let clone = menuTemplate.cloneNode(true).content;
-                    clone.querySelector(".navn").textContent = ret.gsx$navn.$t;
-                    clone.querySelector(".kort").textContent = ret.gsx$kort.$t;
-                    clone.querySelector(".pris").textContent = "Pris: " + ret.gsx$pris.$t + " kr.";
-                    clone.querySelector("img").src = "imgs/small/" + ret.gsx$billede.$t + "-sm.jpg";
-
-                    clone.querySelector("article").addEventListener("click", () => visDetaljer(ret));
-
-                    list.appendChild(clone);
-                    console.log(ret);
-
-                }
-            })
-        }
-
-        function visDetaljer(ret) {
-            console.log(ret);
-            popop.querySelector("h2").textContent = ret.gsx$navn.$t;
-            popop.querySelector("img").src = "imgs/small/" + ret.gsx$billede.$t + "-sm.jpg";
-            popop.querySelector(".pris").textContent = ret.gsx$pris.$t + " kr.";
-            popop.querySelector(".lang").textContent = ret.gsx$lang.$t;
-            popop.style.display = "block";
-
-        }
+document.querySelector("#luk").addEventListener("click", () => popop.style.display = "none");
 
 
-        document.querySelector("#luk").addEventListener("click", () => popop.style.display = "none");
+//funktionen lytter efter om der klikkes på knapperne
+function addEventListenersToButtons() {
+    document.querySelectorAll(".filter").forEach((btn) => {
+        btn.addEventListener("click", filterBTNs);
+    });
+}
 
-        function addEventListenersToButtons() {
-            document.querySelectorAll(".filter").forEach((btn) => {
-                btn.addEventListener("click", filterBTNs);
-            });
-        }
+//funktion vælger hvilke data der skal vises når man trykke på knappen
+function filterBTNs() {
+ filter = this.dataset.kategori;
 
+    document.querySelector("h1").textContent = this.textContent;
+document.querySelectorAll(".filter").forEach((btn) => {
+        btn.classList.remove("valgt");
+})
 
-        function filterBTNs() {
+    this.classList.add("valgt");
 
-            filter = this.dataset.kategori;
-
-            document.querySelector("h1").textContent = this.textContent;
-
-            document.querySelectorAll(".filter").forEach((btn) => {
-                btn.classList.remove("valgt");
-                btn.classList.remove("valgt");
-
-
-            })
-            this.classList.add("valgt");
-            this.classList.add("red");
-
-
-            visRetter();
-        }
+    visSteder();
+}
